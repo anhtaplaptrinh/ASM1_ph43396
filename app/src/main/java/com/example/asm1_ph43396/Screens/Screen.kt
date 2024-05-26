3122d4e439bef7b04dd9c18d565f1bc33c0f6e13
acertuananh <anhttph43396@fpt.edu.vn>
2024-05-26 23:03:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/asm1_ph43396/Screens/Screen.kt
@@ -0,0 +1,14 @@
+package com.example.asm1_ph43396.Screens
+
+enum class Screen(val route : String){
+    WelcomeScreen("WelcomeScreen"),
+    LoginScreen("LoginScreen"),
+    SignUpScreen("SignUpScreen"),
+    HomeScreen("HomeScreen"),
+    DetailProduct("DetailProduct"),
+    CongratScreen("CongratScreen"),
+    CartScreen("CartScreen"),
+    CheckOut("CheckOut")
+
+
+}
